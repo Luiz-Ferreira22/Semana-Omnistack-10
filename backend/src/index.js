d3be8b0a0e5576e3b0c762d8047e91a0a532fdf5
Luiz Ferreira <luizferreira.amazing@outlook.com>
2020-06-18 18:34:43 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://omnistack:2210@cluster0-7ukay.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
